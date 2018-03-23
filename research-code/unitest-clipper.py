ccea98171b128328b15c774cc4cfc9b05f45ff29
lmotnikar <lenart.motnikar@gmail.com>
2018-03-23 16:13:12 +0100

    Dodan newpath

--- research-code/unitest-clipper.py
+++ research-code/unitest-clipper.py
@@ -1,11 +1,12 @@
 import os
 
 mypath = 'path'
+newpath = 'newpath'
 
 for file in os.listdir(mypath):
     write = True
     with open(mypath + file, 'r', encoding = 'UTF-8') as input:
-        with open(mypath + file, 'w', encoding = 'UTF-8') as output:
+        with open(newpath + file, 'w', encoding = 'UTF-8') as output:
             for line in input:
                 if line == 'import unittest\n':
                     write = False
